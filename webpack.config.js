12ffbb0e1ff0b66d0cb69a674cd97dab1933786e
kcabhish <kcabhish@gmail.com>
2023-01-04 20:50:43 -0500

    Asset Module asset/resource

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname,'./dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicpath: '/dist'
     },
     mode: 'none',
     module: {
@@ -12,10 +13,11 @@ module.exports = {
                 test: /\.(png|jpg)$/,
                 /**
                  * There are 4 types of asset types
-                 * 1. asset/resource
-                 * 2. asset/inline
-                 * 3. asset
-                 * 4. asset/source
+                 * 1. asset/resource : emits a separate file and exports the URL. Use while importing large files.
+                 * 2. asset/inline : exports a data URI of the asset. Use while importing small files.
+                 * 3. asset : automatically chooses between exporting a data URI and emitting a separate file. Previously achievable by using url-loader with asset size limit. by default it is set to 8kb,
+                 *      files larger than 8kb are treated as asset/resource and files below 8kb are treated as asset/inline
+                 * 4. asset/source : exports the source code of the asset. Previously achievable by using raw-loader.
                  */
                 type: 'asset/resource'  
             }
