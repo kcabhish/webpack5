27e9e5c9229888abfe530168cae6734794250ed6
kcabhish <kcabhish@gmail.com>
2023-01-04 21:40:52 -0500

    CSS loader

--- src/components/hello-world-button/helloWorldButton.js
+++ src/components/hello-world-button/helloWorldButton.js
@@ -3,11 +3,11 @@ class HelloWorldButton {
     render() {
         const button = document.createElement('button');
         button.innerHTML = 'Hello World';
-        button.classList.add = 'hello-world-button'
+        button.classList.add('hello-world-button');
         button.onclick = () => {
             const p = document.createElement('p');
             p.innerHTML = 'Hola Amigos';
-            p.classList.add('hellp-world-text');
+            p.classList.add('hello-world-text');
             body.appendChild(p);
         }
         const body =  document.querySelector('body');
