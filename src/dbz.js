b54cb475017e35f86ab94db5fec88d9ee48169fd
kcabhish <kcabhish@gmail.com>
2023-01-08 23:50:04 -0500

    adding eslint

--- src/dbz.js
+++ src/dbz.js
@@ -1,6 +1,5 @@
 import Heading from './components/heading/heading';
 import Dbz from './components/dbz/dbz';
-import React from 'react';
 
 const heading = new Heading();
 heading.render('dbz page');
--- src/hello-world.js
+++ src/hello-world.js
@@ -1,12 +1,10 @@
 import HelloWorldButton from './components/hello-world-button/helloWorldButton';
 import Heading from './components/heading/heading';
-import React from 'react';
 
 const helloWorldButton = new HelloWorldButton();
 const heading = new Heading();
 heading.render('hello world');
 helloWorldButton.render();
-
 if (process.env.NODE_ENV === 'production') {
     console.log('Production Mode');
 } else if (process.env.NODE_ENV === 'development') {
